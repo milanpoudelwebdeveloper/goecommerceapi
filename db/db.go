123f3f9f8466a1b80fc4e787df49e8ab3ea74f3f
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-06-27 19:40:30 +0545

    Add database handler in main server

--- db/db.go
+++ db/db.go
@@ -2,15 +2,11 @@ package db
 
 import (
 	"database/sql"
-	"log"
 
 	_ "github.com/lib/pq"
 )
 
 func NewMyPostgresStorage(cfg string) (*sql.DB, error) {
-	db, err := sql.Open("postgres", cfg)
-	if err != nil {
-		log.Fatal("Something went wrong while initializing database", err)
-	}
-	return db, nil
+	return sql.Open("postgres", cfg)
+
 }
