561828659c5af54c6a1e24126981d6db08dea329
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-06-27 19:47:21 +0545

    Add user services routes and http handlers

---
+++ services/user/routes.go
@@ -0,0 +1,27 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Handler struct {
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/login", h.handleLogin)
+	router.HandleFunc("/register", h.handleRegister)
+}
+
+func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+
+}
