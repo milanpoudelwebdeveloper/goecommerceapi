5a9067f049d678a87348e3fa1fcfdcb94428ca15
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-06-28 08:36:14 +0545

    Add utils for decoding and enconding json

--- services/user/routes.go
+++ services/user/routes.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/milanpoudelwebdeveloper/goecommerceapi/types"
+	"github.com/milanpoudelwebdeveloper/goecommerceapi/utils"
 )
 
 type Handler struct {
@@ -23,5 +25,8 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
-
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
 }
